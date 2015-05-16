ff5b5c6a426ae4bceb53f9e6539ab07573bd15ad
tasogare66 <tasogare66@gmail.com>
2015-05-16 18:44:07 +0900

    application 0

---
+++ application_0/application_0/Page1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace application_0
+{
+    /// <summary>
+    /// Page1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+
+            text1.Text = "現在時刻: " + DateTime.Now.ToString();
+        }
+    }
+}
