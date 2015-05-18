46fb9eaf30acfd9753aafb339c847ef6d46cd799
tasogare66 <tasogare66@gmail.com>
2015-05-18 10:12:49 +0900

    2 ライフタイム

--- application_1/myapp.xaml.cs
+++ application_1/myapp.xaml.cs
@@ -7,6 +7,10 @@ namespace EssentialWPF
     {
         public MyApp()
         {
+        }
+
+        void MyApp_Startup(object sender, StartupEventArgs e)
+        {
             Window w = new Window();
             w.Title = "Hello World";
             w.Show();
