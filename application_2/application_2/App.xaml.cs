487f3ad67f774b4387170125a9ca46be021a7d6a
tasogare66 <tasogare66@gmail.com>
2015-05-20 02:57:26 +0900

    "構成状態"

---
+++ application_2/application_2/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace application_2
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        AppSettings _settings = new AppSettings();
+        public AppSettings Settings
+        {
+            get { return _settings; }
+        }
+        
+        public App()
+        {
+            this.Exit += MyApp_Exit;
+            this.Startup += AppStartup;
+        }
+
+        void MyApp_Exit(object sender, ExitEventArgs e)
+        {
+            Settings.Save();
+        }
+
+        void AppStartup(object sender, StartupEventArgs args)
+        {
+            Window w = new Window();
+            w.Title = "実行回数は" + (Settings.RunCount++) + "回です!";
+            w.Show();
+        }
+    }
+}
---
+++ application_2/application_2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace application_2
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class AppSettings : ApplicationSettingsBase
+    {
+        public AppSettings() : base() 
+        {
+        }
+
+        [UserScopedSetting]
+        [DefaultSettingValue("0")]
+        public int RunCount
+        {
+            get { return (int)this["RunCount"];  }
+            set { this["RunCount"] = value;  }
+        }
+    }
+}
