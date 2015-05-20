44c334330c565f8cdba68cd8deaa73b85bc5dbf6
tasogare66 <tasogare66@gmail.com>
2015-05-20 10:16:51 +0900

    InitializeComponent

---
+++ application_3/application_3/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace application_3
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.Startup += AppStartup;
+        }
+
+        void AppStartup(object sender, StartupEventArgs args)
+        {
+            MainWindow w0 = (MainWindow)Application.LoadComponent( new Uri("MainWindow.xaml", UriKind.Relative));
+            w0.Show();
+
+            MainWindow w1 = new MainWindow();
+            w1.Show();
+        }
+
+    }
+}
