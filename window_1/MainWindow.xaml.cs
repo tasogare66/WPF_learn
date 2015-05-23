9e5712617b58abfb6bed74b79207154ac8112898
tasogare66 <tasogare66@gmail.com>
2015-05-24 08:18:00 +0900

    ナビゲーションの制御

---
+++ window_1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace window_1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Navigate(new Page1());
+        }
+    }
+
+    public class Page1 : Page
+    {
+        public Page1()
+        {
+            TextBlock block = new TextBlock();
+            Hyperlink link = new Hyperlink();
+            link.Click += LinkClicked;
+            link.Inlines.Add("クリックしてページ２に移動");
+            block.Inlines.Add(link);
+            Content = block;
+            this.WindowTitle = "ページ１";
+        }
+
+        void LinkClicked(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page2());
+        }
+    }
+
+    public class Page2 : Page
+    {
+        public Page2()
+        {
+            this.WindowTitle = "ページ２";
+        }
+    }
+
+}
---
+++ window_3/name.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace window_3
+{
+    /// <summary>
+    /// name.xaml の相互作用ロジック
+    /// </summary>
+    public partial class name : Page
+    {
+        public name()
+        {
+            InitializeComponent();
+        }
+
+        void Navigate(object sender, RoutedEventArgs e)
+        {
+            var hello = new hello();
+
+            hello.Name = _nameBox.Text;
+            NavigationService.Navigate(hello);
+        }
+
+        NavigationService _navigation;
+
+        private void PageLoaded(object sender, RoutedEventArgs e)
+        {
+            _navigation = this.NavigationService;
+            _navigation.Navigating += Page_Navigating;
+            _navigation.Navigated += Page_Navigated;
+        }
+
+        void Page_Navigated(object sender, NavigationEventArgs e)
+        {
+            _navigation.Navigating -= Page_Navigating;
+            _navigation.Navigated -= Page_Navigated;
+            _navigation = null;
+        }
+
+        void Page_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            if (e.NavigationMode == NavigationMode.New &&
+                _nameBox.Text.Length == 0)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
