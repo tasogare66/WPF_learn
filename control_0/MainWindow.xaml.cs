0ac711675ff11a9b6c564de8bb8a4f5e6790d2ef
tasogare66 <tasogare66@gmail.com>
2015-05-27 08:00:46 +0900

    テンプレート

---
+++ control_0/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace control_0
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StackPanel panel = new StackPanel();
+
+            ContentPresenter intPresenter = new ContentPresenter();
+            intPresenter.Content = 5;
+            panel.Children.Add(intPresenter);
+
+            ContentPresenter datePresenter = new ContentPresenter();
+            datePresenter.Content = DateTime.Now;
+            panel.Children.Add(datePresenter);
+
+            ContentPresenter elementPresenter = new ContentPresenter();
+            elementPresenter.Content = new Button();
+            panel.Children.Add(elementPresenter);
+
+            ListBox l = new ListBox();
+            l.Items.Add("Hello");
+            l.Items.Add("There");
+            l.Items.Add("World");
+            panel.Children.Add(l);
+
+            Content = panel;
+        }
+    }
+}
---
+++ control_1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace control_1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ChangeIt(object sender, RoutedEventArgs e)
+        {
+            ControlTemplate template = new ControlTemplate(typeof(Button));
+            // 不可視の楕円形を作成する テンプレート
+            template.VisualTree = new FrameworkElementFactory(typeof(Ellipse));
+            template.VisualTree.SetValue(Ellipse.FillProperty, Brushes.Blue);
+            template.VisualTree.SetValue(Ellipse.WidthProperty, 75.0);
+            template.VisualTree.SetValue(Ellipse.HeightProperty, 23.0);
+            ((Button)sender).Template = template;
+        }
+    }
+}
